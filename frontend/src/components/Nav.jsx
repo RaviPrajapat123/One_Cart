9caa981e0194e0180fb7b26fbec30abdcd087aa8
RaviPrajapat123 <raviprajapat80031@gmail.com>
2025-07-17 16:14:59 +0530

    Update Nav.jsx

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -19,7 +19,7 @@ function Nav() {
 
   const handleLogOut = async () => {
     try {
-      await axios.get("http://localhost:3000/userLogout", { withCredentials: true });
+      await axios.get("https://one-cart-backend-mene.onrender.com", { withCredentials: true });
       setCurrentUser(null);
       navigate("/login");
       setShowProfile(false);

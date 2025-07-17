06aa99076cd56bd8e8b02c8c92e11c0cf9802849
RaviPrajapat123 <raviprajapat80031@gmail.com>
2025-07-17 18:31:49 +0530

    Update Nav.jsx

--- admin/src/components/Nav.jsx
+++ admin/src/components/Nav.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAdmin } from '../context/authContext'
-
+import logo from "../assets/logo.png"
 function Nav() {
     const navigate=useNavigate()
     const {fetchAdmin}=useAdmin()
@@ -19,7 +19,7 @@ function Nav() {
   return (
     <div className='w-[100vw] h-[70px] bg-[#dcdbdbf8] z-10 fixed top-0 flex items-center justify-between px-[30px] overflow-x-hidden shadow-md shadow-black'>
         <div className='w-[30%] flex items-center justify-start gap-[10px] cursor-pointer' onClick={()=>navigate("/admin/home")}>
-            <img src="../src/assets/logo.png" alt="logo" className='w-[30px]'/>
+            <img src={logo} alt="logo" className='w-[30px]'/>
             <h1 className='text-[25px] text-black font-sans'>OneCart</h1>
         </div>
             <button className='text-[15px] hover:border-[2px] border-[#89daea] cursor-pointer bg-[#000000ca] py-[10px] px-[20px] rounded-2xl text-white' onClick={logOut}>LogOut</button>

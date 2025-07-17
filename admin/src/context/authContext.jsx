5bca8c13ce5d8ecc7f0d089accf72bfcb14fe6a8
RaviPrajapat123 <raviprajapat80031@gmail.com>
2025-07-17 16:29:04 +0530

    Update authContext.jsx

--- admin/src/context/authContext.jsx
+++ admin/src/context/authContext.jsx
@@ -10,7 +10,7 @@ export const AdminProvider = ({ children }) => {
 
   const fetchAdmin = async () => {
     try {
-      const res = await axios.get("http://localhost:3000/currentAdmin", {
+      const res = await axios.get("https://one-cart-backend-mene.onrender.com/currentAdmin", {
         withCredentials: true,
       });
       setAdmin(res.data.admin);
